37713eeb7994d2081887e4a909473a3960c14cc5
shamshuddin <shamshuddinshaik376@gmail.com>
2026-03-08 13:17:42 +0530

    add

--- python/exp14.py
+++ python/exp14.py
@@ -1,35 +1,27 @@
-f=int(input('Enter length in feet:'))
-while True:
-    print('1. Inches')
-    print('2. Yards')
-    print('3. Miles')
-    print('4. Millimeters')
-    print('5. Centimeters')
-    print('6. meters')
-    print('7. Kilometers')
-    print('8. Exit')
-    a=int(input('Enter your choice:'))
-    while True:
-        if a<=8:
-            break
-        else:
-            a=int(input("Enter correct number:"))
-            if a<=8:
-                break
-    if a==1:
-        r=f*12
-    elif a==2:
-        r=f/3.0
-    elif a==3:
-        r=f/5280.0
-    elif a==4:
-        r=f*304.8
-    elif a==5:
-        r=f*30.48
-    elif a==6:
-        r=f*0.3048
-    elif a==7:
-        r=f*0.0003048
-    elif a==8:
-        break
-    print('The required result=',r)
+feet = int(input("Enter feet: "))
+while(True):
+    option = int(input("Enter option to perform conversion: \n1.inches \n2.yards \n3.miles \n4.milli meters \n5.centi meters \n6.meters \n7.kilometers \n"))
+    match option:
+        case 1:
+           r=feet*12
+           print(f"the feet to inches is {r}")
+        case 2:   
+           r=feet/3.0
+           print(f"the feet to yards is {r}")
+        case 3:   
+           r=feet/5280.0
+           print(f"the feet to miles is{r}")
+        case 4:   
+           r=feet*304.8
+           print(f"the feet to milli meters is {r}")
+        case 5:   
+           r=feet*30.48
+           print(f"the feet to centi meters is {r}")
+        case 6:   
+           r=feet*0.3048
+           print(f"the feet to meters is {r}")
+        case 7:   
+           r=feet*0.0003048
+           print(f"the feet to kilo meters is {r}")
+        case 8:
+          break
--- python/exp15.py
+++ python/exp15.py
@@ -1,8 +1,39 @@
+# Way 1
 def sum_digits(n):
-    result = 0
+    s = 0
     while n > 0:
-        result += n % 10
+        s += n % 10
         n //= 10
-    return result
+    return s
 n = int(input("Enter number: "))
 print(sum_digits(n))
+
+
+'''
+1.
+let n = 23 
+s = 0 + 23 % 10 => 3
+n = 23 // 10 => 2
+
+2.
+n = 2 
+s = 3 + 2 % 10 => 3 + 0 => 3
+n = 2 // 10 => 5
+'''
+
+# Way 2
+n1 = int(input("Enter n1: "))
+s = 0
+
+for i in str(n):
+    s = s + int(i)
+print(s)
+
+'''
+1. let n = 56
+i runs at str(56) -> '5'6' -> 0 and 1
+s = 0 + 5 => 5
+2.
+s = 5 + 6 => 11
+-> it prints 11
+'''
